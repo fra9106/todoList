696bcbf67361c3e7264be8c555575329b93d6969
fra9106 <contact@monpersoweb.fr>
2021-02-21 11:53:24 +0100

    register form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -14,7 +15,15 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
+            ->add('first_name', TextType::class, [
+                'label' => "Prénom"
+                ])
+            ->add('last_name', TextType::class, [
+                'label' => "Nom"
+                ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email'
+                ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les deux mots de passe doivent correspondre.',
@@ -22,7 +31,14 @@ class UserType extends AbstractType
                 'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
             ])
-            ->add('email', EmailType::class, ['label' => 'Adresse email'])
+            ->add('roles', ChoiceType::class, [
+                 'label' => 'Role',
+                 'choices' => [
+                     'Admin' => "ROLE_ADMIN",
+                     'User' => "ROLE_USER"
+                 ],
+                 'multiple' => false
+            ])
         ;
     }
 }
