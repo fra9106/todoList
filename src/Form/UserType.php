d728da0c6cc58577aa788ec26431ad5f2275d2f1
fra9106 <contact@monpersoweb.fr>
2021-02-22 08:46:37 +0100

    dataTransformer

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -2,13 +2,17 @@
 
 namespace App\Form;
 
+use App\Entity\User;
+use PhpParser\Builder\Function_;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
@@ -17,13 +21,13 @@ class UserType extends AbstractType
         $builder
             ->add('first_name', TextType::class, [
                 'label' => "Prénom"
-                ])
+            ])
             ->add('last_name', TextType::class, [
                 'label' => "Nom"
-                ])
+            ])
             ->add('email', EmailType::class, [
                 'label' => 'Adresse email'
-                ])
+            ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les deux mots de passe doivent correspondre.',
@@ -32,13 +36,30 @@ class UserType extends AbstractType
                 'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
             ])
             ->add('roles', ChoiceType::class, [
-                 'label' => 'Role',
-                 'choices' => [
-                     'Admin' => "ROLE_ADMIN",
-                     'User' => "ROLE_USER"
-                 ],
-                 'multiple' => false
-            ])
-        ;
+                'label' => 'Role',
+                'choices' => [
+                    'Admin' => "ROLE_ADMIN",
+                    'User' => "ROLE_USER"
+                ],
+                'multiple' => false
+            ]);
+
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+
+                    return count($rolesArray) ? $rolesArray[0] : null;
+                },
+                function ($rolesString) {
+                    return [$rolesString];
+                }
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
     }
 }
