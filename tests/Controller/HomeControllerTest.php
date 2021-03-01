de24cad25a7f60dd32c8ecb1d8991f6ba9bdfcaf
fra9106 <contact@monpersoweb.fr>
2021-03-01 14:27:13 +0100

    hometest

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -13,5 +13,8 @@ class HomeControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSame(1, $crawler->filter('h1')->count());
     }
+
+    
 }
