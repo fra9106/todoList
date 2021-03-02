ae7ab7f3c2586d498bb7cb375e72752a8eb0842a
fra9106 <contact@monpersoweb.fr>
2021-03-02 09:54:39 +0100

    taskTests

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -3,18 +3,31 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testHomePage()
     {
         $client = static::createClient();
+        $client->request('GET', '/');
+       
 
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertSame(1, $crawler->filter('h1')->count());
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);  
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());// idem
+       
     }
 
+    public function testContentPage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSame(1, $crawler->filter('h1')->count());
+        $this->assertSame(4, $crawler->filter('a.btn')->count());
+        $this->assertSelectorTextContains('h1', 'Bienvenue sur Todo List, l\'application vous permettant de gérer l\'ensemble de vos tâches sans effort !');
+        
+
+    }
     
 }
---
+++ tests/Controller/TaskControllersTest.php
@@ -0,0 +1,34 @@
+<?php 
+
+namespace App\Tests\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TaskController extends WebTestCase
+{
+    public function testTaskListNoAurenticated()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/tasks');
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testAuthPageRestricted()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/tasks');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function testRedirectLogin()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/tasks');
+
+        $this->assertResponseRedirects('/login');
+    }
+}
