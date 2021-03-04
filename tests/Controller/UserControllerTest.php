c39dad9455d308b7b9554eb0d1c0f6380c7c23e6
fra9106 <contact@monpersoweb.fr>
2021-03-04 19:54:01 +0100

    UserControllerTest : continuation and end

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserControllerTest extends NeedLogin
 {
+    /**
+     * Test users list
+     *
+     * @return void
+     */
     public function testUserList()
     {
         $this->loginAdmin();
@@ -14,6 +19,11 @@ class UserControllerTest extends NeedLogin
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
     }
 
+    /**
+     * Test create user
+     *
+     * @return void
+     */
     public function testUserCreate()
     {
         $this->loginAdmin();
@@ -32,6 +42,45 @@ class UserControllerTest extends NeedLogin
 		$this->assertResponseRedirects('/login');
 		$this->client->followRedirect();
 		$this->assertSelectorExists('.alert-success');
+    }
+
+    /**
+     * Test user edit
+     *
+     * @return void
+     */
+    public function testUserEdit()
+    {
+        $this->loginAdmin();
+        $crawler = $this->client->request('GET', '/users/3/edit');
+        $form = $crawler->selectButton('Modifier')->form([
+			'user[first_name]' => "John",
+			'user[last_name]' => "Deuf",
+            'user[email]' => "johndeuf@user.fr",
+            'user[password][first]' => "john",
+            'user[password][second]' => "john",
+            'user[roles]' => "ROLE_ADMIN"
+		]);
+        
+        $this->client->submit($form);
+		$this->assertResponseRedirects('/users');
+		$this->client->followRedirect();
+		$this->assertSelectorExists('.alert-success');
+    }
+
+    /**
+     * Test user delete
+     *
+     * @return void
+     */
+    public function testUserDelete()
+    {
+        $this->loginAdmin();
+        $this->client->request('GET', '/users/3/delete');
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        $this->assertResponseRedirects('/users');
+        $this->client->followRedirect();
+        $this->assertSelectorExists('.alert-success');
     }
 
 }
